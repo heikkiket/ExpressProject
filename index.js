27fb276bdd795aff602647560393801536a5509c
Heikki Ketoharju <heikki.ketoharju@iki.fi>
2020-01-30 14:08:21 +0200

    Toimiva server-setup

---
+++ index.js
@@ -0,0 +1,9 @@
+var http = require("http");
+
+http.createServer(function(request, response) {
+    response.writeHead(200, {'Content-type': 'text/plain'});
+
+    response.end('Hello, world!\n');
+}).listen(8081);
+
+console.log('Server running at http://localhost:8081');
