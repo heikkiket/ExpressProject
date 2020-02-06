36d0a65490f075c25d1fad13a867d87dfa5431c2
Heikki Ketoharju <heikki.ketoharju@iki.fi>
2020-02-06 21:22:57 +0200

    Simple mongoDB integration

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
 const app = express();
 const port = 8080;
 
+var url = 'mongodb://levylistauser:levylista@localhost:27017?authSource=admin';
+
+
 app.get("/", (req, res) => {
     console.log("Got response!");
-    res.send("Hello, world!");
+
+    MongoClient.connect(url, (err, connection) => {
+        if (err) throw err;
+
+        db = connection.db("levylista");
+        db.collection("levyt").find({}).toArray((err, result) => {
+            if (err) throw err;
+            res.send(result);
+        });
+        connection.close();
+    });
 });
 
 app.listen(port, () => console.log(`Example app listening at localhost on port ${port}`));
