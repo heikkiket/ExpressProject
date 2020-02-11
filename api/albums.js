a26d1737a1a2e84e7f3ae76265fe4203115631a0
Heikki Ketoharju <heikki.ketoharju@iki.fi>
2020-02-11 22:34:31 +0200

    Working simple backend

---
+++ api/albums.js
@@ -0,0 +1,23 @@
+const ObjectId = require('mongodb').ObjectId;
+
+module.exports = {
+    db: null,
+
+    setDb: function(db) {
+        this.db = db;
+    },
+
+    getAll: async function() {
+        let result = await db.collection("levyt").find({}).toArray();
+        return result;
+    },
+
+    getSingle: async function(id) {
+        let query = {_id: ObjectId(id)};
+        let result = await db.collection("levyt").find(query).toArray();
+        return result;
+    },
+
+    search: function(q) {
+    }
+}
---
+++ api/index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const Albums = require('./albums.js');
+const Artists = require('./artists.js');
+const MongoClient = require('mongodb').MongoClient;
+
+const app = express();
+const port = 8080;
+
+var url = 'mongodb://levylistauser:levylista@localhost:27017?authSource=admin';
+
+
+    MongoClient.connect(url, (err, connection) => {
+        if (err) throw err;
+
+        db = connection.db("levylista");
+        Albums.setDb(db);
+        Artists.setDb(db);
+
+        app.get("/", (req, res) => {
+            res.sendFile(__dirname + '/vue/index.html');
+        });
+
+        app.get("/api/albums", async (req, res) => {
+            result = await Albums.getAll();
+            res.send(result);
+
+        });
+
+        app.get("/api/albums/:id", async (req, res) => {
+            res.send(await Albums.getSingle(req.params.id));
+        });
+
+        app.get("/api/artists", async (req, res) => {
+            res.send(await Artists.getAll());
+        });
+
+
+        app.get("/api/artists/:id", async (req, res) => {
+            let result = await Artists.getSingle(req.params.id)
+                .catch((err) => {console.log(err);});
+            res.send(result);
+        });
+
+
+        app.listen(port, () => console.log(`Example app listening at localhost on port ${port}`));
+
+    });
